9b5c4ccff32c8f219a0f3cb4d47f396a1dd0e75f
hofmadresu <hofmadresu@gmail.com>
2024-07-07 19:36:54 -0400

    Lower boombox volume

--- HofmaDresuBoomboxPlaylist/Patches/BoomboxItem_Start.cs
+++ HofmaDresuBoomboxPlaylist/Patches/BoomboxItem_Start.cs
@@ -5,8 +5,17 @@ namespace HofmaDresuBoomboxPlaylist.Patches
     [HarmonyPatch(typeof(BoomboxItem), "Start")]
     internal class BoomboxItem_Start
     {
+        const float targetVolume = 0.4f;
+
         static void Postfix(BoomboxItem __instance)
         {
+            if (__instance.boomboxAudio.volume != targetVolume)
+            {
+               __instance.boomboxAudio.volume = targetVolume;
+                HofmaDresuBoomboxPlaylist.LogInfo($"volume set to {targetVolume}");
+            }
+
+
             if (AudioManager.FinishedLoading)
                 AudioManager.ApplyClips(__instance);
             else

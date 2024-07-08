ee8bf9d6c8e85aaec1a19a1d698c4d99c3713f79
hofmadresu <hofmadresu@gmail.com>
2024-07-07 20:03:35 -0400

    volume per song

---
+++ HofmaDresuBoomboxPlaylist/Patches/BoomboxItem_StartMusic.cs
@@ -0,0 +1,28 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HofmaDresuBoomboxPlaylist.Patches
+{
+    [HarmonyPatch(typeof(BoomboxItem), "StartMusic")]
+    internal class BoomboxItem_StartMusic
+    {
+        static void Postfix(BoomboxItem __instance, bool startMusic)
+        {
+            if (startMusic)
+            {
+                switch(__instance.boomboxAudio.clip.name)
+                {
+                    case "Bitter Regret.mp3":
+                        __instance.boomboxAudio.volume = 0.6f;
+                        break;
+                    case "Lethal Company Blues.mp3":
+                        __instance.boomboxAudio.volume = 0.4f;
+                        break;
+                }
+                HofmaDresuBoomboxPlaylist.LogInfo($"Playing {__instance.boomboxAudio.clip.name} at volume {__instance.boomboxAudio.volume}");
+            }
+        }
+    }
+}
